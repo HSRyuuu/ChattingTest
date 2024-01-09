006fa758ac75a1cb17957a474d7063d6caec5102
HyunSik Ryu <hsryu123@naver.com>
2024-01-09 15:44:05 +0900

    initial commit

---
+++ src/test/java/com/example/chattingtest/ChatMessageTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chattingtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatMessageTestApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
